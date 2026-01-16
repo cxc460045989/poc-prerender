83aba93bce7b0d80607855aa84015fab406f7731
cxc460045989 <460045989@qq.com>
2026-01-16 11:41:58 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,13 @@
 import type { Metadata } from 'next';
 
 // 【可选】在layout中定义全局默认的Metadata（推荐，替代手动写的静态标签）
-export const metadata: Metadata = {
-  // 全局默认的基础配置（page级会覆盖这些）
-  openGraph: {
-    type: 'website', // 全局默认的og:type
-    url: 'https://poc-prerender-beta.vercel.app/', // 全局默认的og:url
-  },
-};
+// export const metadata: Metadata = {
+//   // 全局默认的基础配置（page级会覆盖这些）
+//   openGraph: {
+//     type: 'website', // 全局默认的og:type
+//     url: 'https://poc-prerender-beta.vercel.app/', // 全局默认的og:url
+//   },
+// };
 
 export default function RootLayout({
   children,
